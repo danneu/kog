92ca978ffa11976ff76a0dfd606512b2a6e23201
Dan Neumann <danrodneu@gmail.com>
2016-12-11 10:01:56 -0600

    Update main

--- src/main/kotlin/com/danneu/kog/Main.kt
+++ src/main/kotlin/com/danneu/kog/Main.kt
@@ -4,31 +4,8 @@ package com.danneu.kog
 class Main {
     companion object {
         @JvmStatic fun main(args: Array<String>) {
-            val router = Router {
-                get("/") {
-                    Response()
-                      .setHeader(Header.AccessControlAllowOrigin, "*")
-                      .text("Hello, World!")
-                }
-                websocket("/foo") { request: Request, socket: WebSocket ->
-                    println("[foo] a client connected")
-                    socket.onClose = { statusCode, reason -> println("[foo] a client disconnected") }
-                    socket.onText = { text ->
-                        println("[foo] said $text")
-                        socket.session.remote.sendString(text)
-                    }
-                }
-                websocket("/bar") { request: Request, socket: WebSocket ->
-                    println("[bar] a client connected")
-                    socket.onClose = { statusCode, reason -> println("[bar] a client disconnected") }
-                    socket.onText = { text ->
-                        println("[bar] said $text")
-                        socket.session.remote.sendString(text)
-                    }
-                }
-            }
-            val server = Server(router.handler())
-            server.listen(9000)
+            val handler: Handler = { Response().text("Hello, world!") }
+            Server(handler).listen(Env.int("PORT") ?: 3000)
         }
     }
 }
